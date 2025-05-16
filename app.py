796d7368302cf51f7c789c6568c1a1e07c01c75c
MorganClemens <morganclemens1@gmail.com>
2025-05-16 10:41:53 -0700

    Build execute_command method to handle user commands in the terminal. Add quit() functionality (will need to verify this works as intended later).

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ class App():
 
     def __init__(self):
         self.toDoList = models.ToDoList() # create new to do list object
+        self.working_item = "" # stores the current working item
 
     def read_input(self):
         '''Prompts and parses user input. Returns int corresponding to command.'''
@@ -13,7 +14,7 @@ class App():
         # Parse user input
         user_input_split = user_input.split()
         user_command = user_input_split[0]
-        user_item  = user_input_split[-1]
+        self.working_item  = user_input_split[-1] # store the user item as current working item
 
         # Handle invalid input
         valid_commands = ["add", "finish", "delete", "quit"]
@@ -34,6 +35,24 @@ class App():
                 return 3
             case _:
                 return -1
+            
+    def execute_command(self, command_index):
+        '''Reads the command index and executes the corresponding command.'''
+        match command_index:
+            case 0:
+                self.toDoList.add_item(self.working_item) # add current working item to the to-do list
+            case 1:
+                self.toDoList.finish_item(self.working_item) # finish current working item from to-do list
+            case 2:
+                self.toDoList.remove_item(self.working_item) # delete current working item from to-do list
+            case 3:
+                quit() # quits the session - will need to verify this works later
+            case -1:
+                print("Unknown error.")
+
+            
+
+        
 
 
 
